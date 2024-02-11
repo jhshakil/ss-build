d8dddbcc0e1ede56d5c4b9a8ea5e89335eddb793
jhshakil <shakil11271@gmail.com>
2024-02-11 10:39:40 +0600

    add light and dark mode

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "@/components/global/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
@@ -28,6 +30,8 @@ const Navigation = ({ user }: Props) => {
         >
           Login
         </Link>
+        <UserButton />
+        <ModeToggle />
       </aside>
     </div>
   );
