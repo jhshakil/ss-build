7d414c1c39c1430a3f9a0392c84ddaf6f6775609
jhshakil <shakil11271@gmail.com>
2024-03-09 20:58:05 +0600

    working on billing page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -45,7 +45,7 @@ export const pricingCards = [
     duration: "month",
     highlight: "Key features",
     features: ["Rebilling", "24/7 Support team"],
-    priceId: "price_1OYxkqFj9oKEERu1KfJGWxgN",
+    priceId: "price_1OsQgvGxt14VauoBNtKnJytC",
   },
   {
     title: "Basic",
@@ -54,8 +54,12 @@ export const pricingCards = [
     duration: "month",
     highlight: "Everything in Starter, plus",
     features: ["Unlimited Sub accounts", "Unlimited Team members"],
-    priceId: "price_1OYxkqFj9oKEERu1NbKUxXxN",
+    priceId: "price_1OsQeCGxt14VauoBDzuCTkaR",
   },
+];
+
+export const addOnProducts = [
+  { title: "Priority Support", id: "prod_PhqegkD4U8WI0d" },
 ];
 
 export const icons = [
